9e8204788a4baa07477a157af269f57a04c5bf64
John Robert Capistrano <johnrobertcapistrano@Yondus-MacBook-Air.local>
2025-05-18 13:59:26 +0800

    @update: set database connection using postgres

---
+++ app/go-breeders/cmd/web/db.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	maxOpenDBConn = 25
+	maxIdleDBConn = 25
+	maxDbLifetime = 5 * time.Minute
+)
+
+func initPostgresDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxOpenDBConn)
+	db.SetConnMaxIdleTime(maxIdleDBConn)
+	db.SetConnMaxLifetime(maxDbLifetime)
+
+	return db, nil
+}
